8c5b1f8ac2236d7f1bd8094264c8a9c3b4114c23
MahdiSolgi <vass1383@gmail.com>
2025-02-20 16:14:44 -0800

    feat([id]/page): create page.tsx

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Ad from "../_components/Ad";
+
+const page: FC = () => {
+  return (
+    <div className=" mt-28">
+      <Ad />
+    </div>
+  );
+};
+
+export default page;
