ebea043abb6a6d5c4068dad61093b3ac691cbd14
MahdiSolgi <vass1383@gmail.com>
2025-02-27 23:30:53 -0800

    feat(create-Ad/page): add AdCat component to page

---
+++ src/app/create-Ad/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import AdCat from "../_components/AdCat";
+
+const page: FC = () => {
+  return (
+    <div>
+      <AdCat />
+    </div>
+  );
+};
+
+export default page;
