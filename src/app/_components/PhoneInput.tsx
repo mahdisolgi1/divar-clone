90147f4697ab87d9b8015488c7e195b1e3705024
MahdiSolgi <vass1383@gmail.com>
2025-03-12 13:57:58 -0700

    feat(PhoneInput): create PhoneInput component

---
+++ src/app/_components/PhoneInput.tsx
@@ -0,0 +1,71 @@
+import { ChangeEvent } from "react";
+
+interface PhoneInputProps {
+  phoneNumber: string;
+  onPhoneChange: (phoneNumber: string) => void;
+}
+
+const PhoneInput: React.FC<PhoneInputProps> = ({
+  phoneNumber,
+  onPhoneChange,
+}) => {
+  const toPersianNumbers = (num: string): string => {
+    const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    return num.replace(/\d/g, (match) => persianNumbers[parseInt(match)]);
+  };
+
+  const formatPhoneNumber = (num: string): string => {
+    const cleaned = num.replace(/\D/g, "");
+
+    const match = cleaned.match(/^(\d{4})(\d{3})(\d{4})$/);
+    if (match) {
+      return `${match[1]} ${match[2]} ${match[3]}`;
+    }
+    return cleaned;
+  };
+
+  const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    const cleaned = input.replace(/\D/g, "").slice(0, 11);
+    onPhoneChange(cleaned);
+  };
+
+  return (
+    <div className="w-full me-auto text-right flex gap-1 flex-col">
+      <h3 className="text-base text-black-primary pr-1">شماره تلفن</h3>
+      <div className="relative">
+        <input
+          type="tel"
+          value={phoneNumber ?? ""}
+          onChange={handlePhoneChange}
+          className="bg-white border hover:border-black-dark-100 border-black-medium-100 w-full mx-auto rounded-lg px-2 py-2 text-black placeholder:text-right text-right pl-10 appearance-none no-spinner"
+          placeholder="شماره تلفن"
+        />
+      </div>
+
+      {phoneNumber && (
+        <div className="flex justify-end gap-1">
+          <span className="text-base text-black-primary text-right mt-1">
+            {toPersianNumbers(formatPhoneNumber(phoneNumber))}
+          </span>
+          <span className="text-base text-black-primary text-right mt-1">
+            :شماره تلفن
+          </span>
+        </div>
+      )}
+
+      <style jsx>{`
+        .no-spinner::-webkit-inner-spin-button,
+        .no-spinner::-webkit-outer-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+        .no-spinner {
+          -moz-appearance: textfield;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default PhoneInput;
