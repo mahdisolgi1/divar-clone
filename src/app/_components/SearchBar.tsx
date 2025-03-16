8d42e52e5c6ffe88d2934f86666aba2984de09bb
MahdiSolgi <vass1383@gmail.com>
2025-03-16 06:02:13 -0700

    feat(SearchBar): create SearchBar component to handle searching for ads

---
+++ src/app/_components/SearchBar.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { useState, useEffect } from "react";
+import { IoIosSearch } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Ad } from "../_types/modalTypes";
+import { searchAds } from "../_lib/data-service";
+import Link from "next/link";
+
+const SearchBar: React.FC = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState<Ad[]>([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const province = searchParams.get("province") || "";
+  useEffect(() => {
+    const fetchResults = async () => {
+      if (query.trim()) {
+        const ads = await searchAds(query, province);
+        setResults(ads);
+        setShowDropdown(true);
+      } else {
+        setResults([]);
+        setShowDropdown(false);
+      }
+    };
+
+    const timeout = setTimeout(fetchResults, 300);
+    return () => clearTimeout(timeout);
+  }, [query, province]);
+
+  const handleRemoveProvince = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.delete("query");
+    router.push(url.toString());
+
+    setQuery("");
+    setShowDropdown(false);
+  };
+
+  const handleProvinceSelect = (query: string) => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("query", query);
+
+    router.push(url.toString());
+  };
+  return (
+    <div className="relative w-full max-w-md">
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+        className="bg-[#f0f0f1] pr-8 rounded-lg px-4 py-2 md:w-[25rem] lg:w-[30rem] text-black placeholder:text-right text-right pl-10"
+        placeholder="جستجو در همه آگهی ها"
+      />
+      <IoIosSearch
+        onClick={() => router.push(`/search?query=${query}`)}
+        className="absolute cursor-pointer left-3 top-1/2 transform -translate-y-1/2 text-black-secondary text-xl"
+      />
+
+      {query && (
+        <IoClose
+          className="absolute right-[-1.5rem] top-1/2 transform -translate-y-1/2 text-black-secondary cursor-pointer hover:text-black-primary"
+          size={18}
+          onClick={handleRemoveProvince}
+        />
+      )}
+
+      {showDropdown && results.length > 0 && (
+        <div className="absolute p-4 md:w-[25rem] gap-2  lg:w-[30rem] bg-white border border-gray-300 rounded-lg mt-1 shadow-lg z-10 max-h-60 overflow-y-auto">
+          {results.map((ad) => (
+            <Link
+              href={`/${ad.id.toString()}`}
+              key={ad.id}
+              className="px-4 text-black-secondary py-2 text-right  cursor-pointer hover:bg-gray-100"
+              onClick={() => setShowDropdown(false)}
+            >
+              {ad.title}
+            </Link>
+          ))}
+          <button
+            className="w-full text-black-secondary text-right py-2  hover:bg-gray-300"
+            onClick={() => handleProvinceSelect(query)}
+          >
+            مشاهده همه نتایج
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
