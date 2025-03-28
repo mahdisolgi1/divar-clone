40c9ef1af9e4ace5b94fb055106077c9e5397964
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-03-28 06:06:01 -0700

    feat(page): get prefered province from user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import Gallery from "@/app/_components/Gallery";
+"use client";
+import ProvinceSelector from "./_components/ProvinceSelector";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-const page: React.FC = () => {
+const Page: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const savedProvince = localStorage.getItem("selectedProvince");
+    if (savedProvince) {
+      router.push(`/ads?province=${savedProvince}`);
+    }
+  }, [router]);
+
   return (
-    <div className=" mt-28 ">
-      <Gallery />
+    <div className="mt-28">
+      <ProvinceSelector />
     </div>
   );
 };
 
-export default page;
+export default Page;
