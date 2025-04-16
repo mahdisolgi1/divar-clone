6aec37577abda26f35cd78fb4bb682edf26693bc
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-04-16 07:47:21 -0700

    feat(AuthModals): create AuthModals component to handle login and signup modals

---
+++ src/app/_components/AuthModals.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import LoginModal from "./LoginModal";
+import SignUpModal from "./SignUpModal";
+interface AuthModalsProps {
+    open: boolean;
+    handleClose: () => void;
+  }
+  const AuthModals = ({open ,handleClose}:AuthModalsProps)        => {
+    const [openLogin, setOpenLogin] = useState(true);
+    const [openSignUp, setOpenSignUp] = useState(false);
+  
+    return (
+      <>{open &&(<>
+        <LoginModal
+          open={openLogin}
+          handleClose={() => setOpenLogin(false)}
+          openSignUp={() => {
+            setOpenLogin(false);
+            setOpenSignUp(true);
+          }}
+        />
+        <SignUpModal
+          open={openSignUp}
+          handleClose={() => setOpenSignUp(false)}
+          openLogin={() => {
+            setOpenSignUp(false);
+            setOpenLogin(true);
+        }}
+        />
+        </>
+        )}
+      </>
+    );
+  };
+  export default AuthModals;
