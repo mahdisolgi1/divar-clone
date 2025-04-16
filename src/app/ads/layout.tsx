c2fbf345f09cb2361b34adae5348cd60754c4785
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-04-16 09:10:14 -0700

    feat(ads/layout): add user provider to ads layout

--- src/app/ads/layout.tsx
+++ src/app/ads/layout.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import Navbar from "../_components/Navbar";
+import { UserProvider } from "../_context/UserContext";
 import "../globals.css";
-import Navbar from "@/app/_components/Navbar";
 
 export default function AdsLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <>
-      <Navbar />
-      <main className="mt-28">{children}</main>
-    </>
+    <div>
+      <UserProvider>
+        <Navbar />
+        <main className="mt-28 px-4">{children}</main>
+      </UserProvider>
+    </div>
   );
 }
