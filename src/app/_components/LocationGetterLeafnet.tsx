094d5fd5b59320abf85e76dc1df355f0d44171d1
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-07-14 05:18:52 -0700

    fix(LocationGetterLeafnet): fix error related to marker

--- src/app/_components/LocationGetterLeafnet.tsx
+++ src/app/_components/LocationGetterLeafnet.tsx
@@ -1,54 +1,66 @@
-import { useState } from "react";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 const icon = L.icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
 });
 
 interface MapProps {
-  selectedProvince: [number, number];
+  selectedCity: [number, number];
   onPosition: (position: [number, number]) => void;
 }
 
-const CenterMarker = ({
-  onPosition,
-}: {
-  onPosition: (position: [number, number]) => void;
-}) => {
+const CenterMarker = ({ onPosition }: { onPosition: (position: [number, number]) => void }) => {
   const [position, setPosition] = useState<[number, number]>([0, 0]);
+  const map = useMap();
+
+  useEffect(() => {
+    if (position[0] === 0 && position[1] === 0) {
+      const center = map.getCenter();
+      const newPosition: [number, number] = [center.lat, center.lng];
+      setPosition(newPosition);
+      onPosition(newPosition);
+    }
+  }, []);
 
   useMapEvents({
+    click: (e) => {
+      const newPosition: [number, number] = [e.latlng.lat, e.latlng.lng];
+      setPosition(newPosition);
+      onPosition(newPosition);
+    },
     moveend: (e) => {
       const center = e.target.getCenter();
       const newPosition: [number, number] = [center.lat, center.lng];
-      if (position[0] !== newPosition[0] || position[1] !== newPosition[1]) {
-        setPosition(newPosition);
-        onPosition(newPosition);
-      }
+      setPosition(newPosition);
+      onPosition(newPosition);
     },
   });
 
   return <Marker position={position} icon={icon} />;
 };
-
-export default function LeafletMap({ selectedProvince, onPosition }: MapProps) {
+export default function LeafletMap({ selectedCity, onPosition }: MapProps) {
   return (
-    <MapContainer
-      center={selectedProvince}
-      zoom={12}
-      zoomControl={false}
-      className="w-full h-[400px] relative -z-10"
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
-      <CenterMarker onPosition={onPosition} />
-    </MapContainer>
+    <div className="w-full h-[400px] relative">
+      <MapContainer
+        center={selectedCity}
+        zoom={12}
+        zoomControl={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        className="w-full h-full"
+        style={{ zIndex: 1 }}  // Remove negative z-index
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+        <CenterMarker onPosition={onPosition} />
+      </MapContainer>
+    </div>
   );
 }
