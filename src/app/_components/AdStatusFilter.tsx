9f06e1509236adf9d33ed0d6526f47d335a94aa8
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-07-14 03:47:31 -0700

    feat(AdStatusFilter): add styles

--- src/app/_components/AdStatusFilter.tsx
+++ src/app/_components/AdStatusFilter.tsx
@@ -16,14 +16,14 @@ const AdStatusFilter: React.FC<AdStatusFilterProps> = ({
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div className="w-full border-b z-40 cursor-pointer border-[#dbdbe4]">
+    <div className="w-full border-b z-40 cursor-pointer  border-[#858585] dark:border-[#2c2c2c]">
       <div
         onClick={() => setIsOpen(!isOpen)}
         className="text-black-primary p-4 text-right flex justify-end items-center gap-2 font-medium cursor-pointer"
       >
         {(Boolean(status) || isExchangeOpen) && (
           <span
-            className="hover:bg-[rgba(166,38,38,0.08)] p-2 absolute left-2 top-1/5 rounded-full text-xs text-brand"
+            className="hover:bg-[rgba(166,38,38,0.08)] p-2 absolute left-2 top-1/5 rounded-full text-xs text-brand dark:text-dark-brand"
             onClick={() => {
               onIsExchangeOpen(false);
               onStatus("");
@@ -33,9 +33,9 @@ const AdStatusFilter: React.FC<AdStatusFilterProps> = ({
           </span>
         )}
 
-        <span className="text-xs">وضعیت آگهی</span>
+        <span className="text-xs  text-black-primary dark:text-dark-white-primary">وضعیت آگهی</span>
         <MdKeyboardArrowDown
-          className={`text-[0.875rem] text-lg transition-transform duration-300 ${
+          className={`text-[0.875rem]  text-black-primary dark:text-dark-white-primary text-lg transition-transform duration-300 ${
             isOpen ? "rotate-180" : ""
           }`}
         />
@@ -59,9 +59,9 @@ const AdStatusFilter: React.FC<AdStatusFilterProps> = ({
                 className="sr-only peer"
               />
 
-              <div className="w-12 h-6 bg-black-medium-200 rounded-full peer-checked:bg-brand transition-colors duration-300 relative">
+              <div className="w-12 h-6 bg-black-medium-200 dark:bg-dark-white-medium-100 rounded-full peer-checked:bg-brand transition-colors duration-300 relative">
                 <div
-                  className={`absolute top-1 right-1 w-4 h-4 bg-white rounded-full shadow-sm transition-transform duration-300 ${
+                  className={`absolute top-1 right-1 w-4 h-4 bg-white dark:bg-dark-gray-750 rounded-full shadow-sm transition-transform duration-300 ${
                     isExchangeOpen ? "-translate-x-6" : "translate-x-0"
                   }`}
                 />
