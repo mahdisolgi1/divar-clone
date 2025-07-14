29d2d8352edbb6b4d04f0af0c33b7cf822bc9759
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-07-14 04:20:34 -0700

    fix(CreateAt): fix error related to category

--- src/app/_components/CreateAd.tsx
+++ src/app/_components/CreateAd.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import React from "react";
 
 import { MdOutlineHomeWork } from "react-icons/md";
 import {
@@ -23,8 +24,9 @@ import StatusDropDown from "./StatusDropDown";
 import PhoneInput from "./PhoneInput";
 import { Button } from "@mui/material";
 import OTECheckBox from "./OTECheckBox";
-import { createAd } from "../_lib/data-service";
-import { Ad } from "../_types/modalTypes";
+import { createAd, getCatesByID } from "../_lib/data-service";
+import { Ad, Category } from "../_types/modalTypes";
+import { useUser } from "../_context/UserContext";
 
 const iconMap: Record<string, React.FC<{ className?: string }>> = {
   املاک: MdOutlineHomeWork,
@@ -39,14 +41,12 @@ const iconMap: Record<string, React.FC<{ className?: string }>> = {
   "استخدام و کاریابی": PiToolboxLight,
 };
 
-const categories = Object.keys(iconMap);
 
 const CreateAd: FC = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState<number>(0);
-  const [place, setPlace] = useState("");
-  const [province, setProvince] = useState<string>("");
+  const [placeID, setPlaceID] = useState<number | undefined>(undefined);
   const [positon, setPosition] = useState<[number, number]>([32.4279, 53.688]);
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [status, setStatus] = useState<string>("");
@@ -55,33 +55,47 @@ const CreateAd: FC = () => {
   const [img3, setImg3] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [isOTEChecked, setIsOTEChecked] = useState<boolean>(false);
-  console.log(`img1=${img1}`);
-  console.log(`img2=${img2}`);
-  console.log(`img3=${img3}`);
+  const [categoryDetail, setCategoryDetail] = useState<Category | null>(null);
   const { cat } = useParams();
-  const decodedCat = cat && decodeURIComponent(cat.toString());
+  const { user } = useUser();
+useEffect(()=>{console.log(user)},[user]);
   const maxLength = 200;
 
-  const category = decodedCat && categories.find((c) => c === decodedCat);
-  const IconComponent = category ? iconMap[category] : null;
-
+useEffect(()=>{ 
+  if (!cat) return;
+  const fetchCategory = async () => {
+    try {
+      const category = await getCatesByID(Number(cat));
+      setCategoryDetail(category);
+      console.log(Number(cat));
+    } catch (error) {
+      console.error("Error fetching category:", error);
+    }
+  };
+  fetchCategory();
+},[cat]);
   const handleSubmit = async () => {
+    if (!placeID) {
+      alert("Please select a location");
+      return;
+    }
     const newAd = {
       title,
       description,
       price,
       phoneNumber: Number(phoneNumber),
-      place,
-      province,
+      placeID,
       latitude: Number(positon[0]),
       longitude: Number(positon[1]),
-      category: decodedCat,
+      categoryID: Number(cat),
       status,
       img1,
       img2,
       img3,
       openToExchange: isOTEChecked,
-    } as Ad;
+      userID: user?.id,
+      userEmail: user?.email,
+    };
 
     try {
       setLoading(true);
@@ -96,21 +110,25 @@ const CreateAd: FC = () => {
   return (
     <div className="flex justify-center items-end gap-5 w-full max-w-[40rem] mx-auto flex-col pb-10">
       <h2 className="text-xl font-medium text-gray">ثبت آگهی</h2>
-      <span className="text-[1.125rem] text-black-primary">دستهٔ آگهی</span>;
+      <span className="text-[1.125rem] text-black-primary dark:text-dark-white-primary">دستهٔ آگهی</span>;
       <div className="flex justify-between border-b  w-full flex-row-reverse  p-4">
         <div className="flex justify-center gap-5 flex-row-reverse">
-          {IconComponent && (
-            <IconComponent className="text-black-secondary text-2xl" />
+          {categoryDetail?.category && (
+            <div className="text-black-secondary dark:text-dark-white-secondary text-2xl">
+              {iconMap[categoryDetail.category] && 
+                React.createElement(iconMap[categoryDetail.category], { className: "text-black-secondary dark:text-dark-white-secondary text-2xl" })
+              }
+            </div>
           )}
           <div className="flex flex-col justify-between items-center">
             <span className="text-content-primary text-[0.875rem]">
-              {decodedCat}
+              {categoryDetail?.category}
             </span>
 
             <div className="flex">
-              <MdOutlineKeyboardArrowLeft className="text-black-secondary w-5" />
+              {/* <MdOutlineKeyboardArrowLeft className="text-black-secondary dark:text-dark-white-secondary w-5" /> */}
               <span className="text-content-secondary text-xs">
-                {decodedCat}
+                {categoryDetail?.subCategory2}
               </span>
             </div>
           </div>
@@ -119,37 +137,32 @@ const CreateAd: FC = () => {
           href="/create-Ad"
           className="flex justify-center gap-2 items-center cursor-pointer "
         >
-          <MdOutlineKeyboardArrowLeft className="text-black-secondary text-[0.875rem]" />
-          <span className="text-black-secondary text-[0.875rem]">
-            تغییر دسته
+          <MdOutlineKeyboardArrowLeft className="text-black-secondary dark:text-dark-white-secondary text-[0.875rem]" />
+          <span className="text-black-secondary dark:text-dark-white-secondary text-[0.875rem]">
+            تغییر دسته 
           </span>
         </Link>
       </div>
-      <ProvinceDropDown onProvince={setProvince} onPosition={setPosition} />
-      <div className="w-full me-auto text-right flex gap-1 flex-col">
-        <h3 className="text-base text-black-primary  pr-1">محله</h3>
-        <input
-          type={place}
-          onChange={(e) => setPlace(e.target.value)}
-          className="bg-white border hover:border-black-dark-100 border-black-medium-100  w-full mx-auto rounded-lg px-2 py-2  text-black placeholder:text-right text-right pl-10 "
-          placeholder="محله"
-        />
-      </div>
+      <ProvinceDropDown 
+        onPosition={setPosition} 
+        onPlaceID={setPlaceID}
+      />
+      
       <div className="w-full text-right  items-end flex gap-2 flex-col">
-        <h3 className="text-base text-black-primary  pr-1">* عکس آگهی</h3>
+        <h3 className="text-base text-black-primary dark:text-dark-white-primary  pr-1">* عکس آگهی</h3>
         <ImageUploader onImg1={setImg1} onImg2={setImg2} onImg3={setImg3} />
-        <span className="text-base text-black-secondary">
+        <span className="text-base text-black-secondary dark:text-dark-white-secondary">
           تعداد عکس‌های انتخاب شده نباید بیشتر ۳ از باشد.
         </span>
       </div>
       <PriceInput onPriceChange={setPrice} price={price} />
       <div className="w-full me-auto text-right flex gap-1 flex-col">
-        <h3 className="text-base text-black-primary  pr-1">عنوان</h3>
+        <h3 className="text-base text-black-primary dark:text-dark-white-primary  pr-1">عنوان</h3>
         <input
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="bg-white border hover:border-black-dark-100 border-black-medium-100  w-full mx-auto rounded-lg px-2 py-2  text-black placeholder:text-right text-right pl-10 "
+          className="bg-white border hover:border-black-dark-100 border-black-medium-100 dark:border-dark-white-medium-100 w-full mx-auto rounded-lg px-2 py-2  text-black placeholder:text-right text-right pl-10 "
           placeholder="عنوان"
         />
       </div>
@@ -158,13 +171,13 @@ const CreateAd: FC = () => {
         onIsOTEcheckbox={setIsOTEChecked}
       />
       <div className="w-full me-auto text-right flex gap-1 flex-col">
-        <h3 className="text-base text-black-primary pr-1">توضیحات</h3>
+        <h3 className="text-base text-black-primary dark:text-dark-white-primary pr-1">توضیحات</h3>
         <textarea
           rows={3}
           maxLength={maxLength}
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="bg-white border hover:border-black-dark-100 border-black-medium-100 w-full mx-auto rounded-lg px-2 py-2 text-black placeholder:text-right text-right resize-none"
+          className="bg-white border hover:border-black-dark-100 border-black-medium-100 dark:border-dark-white-medium-100 w-full mx-auto rounded-lg px-2 py-2 text-black placeholder:text-right text-right resize-none"
           placeholder="توضیحات"
         />
       </div>
@@ -184,7 +197,7 @@ const CreateAd: FC = () => {
           <Button
             variant="contained"
             sx={{ background: "white", color: " rgba(0, 0, 0, 0.56)" }}
-            className="hover:bg-black/5 text-black-secondary hover:shadow-none  whitespace-nowrap bg-transparent text-content-primary"
+            className="hover:bg-black/5 text-black-secondary dark:text-dark-white-secondary hover:shadow-none  whitespace-nowrap bg-transparent text-content-primary"
           >
             انصراف
           </Button>
