b4cc91d5620a1dfcab32ca5bef308419947f68f0
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-07-14 04:07:39 -0700

    styles(CheckBox): add label with styles

--- src/app/_components/CheckBox.tsx
+++ src/app/_components/CheckBox.tsx
@@ -10,7 +10,7 @@ const CheckBoxes: React.FC = () => {
 
   return (
     <div onChange={handleChange}>
-      <label className="text-black-primary text-[0.875rem]">
+      <label className="text-black-primary dark:text-dark-white-primary text-[0.875rem]">
         مایلم معاوضه کنم
       </label>
       <Checkbox
