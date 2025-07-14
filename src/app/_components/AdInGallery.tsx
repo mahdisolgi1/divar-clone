dae5087f734968989a0646e32e0829f6e3ec8977
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-07-14 03:43:22 -0700

    styles(AdInGallery): add styles

--- src/app/_components/AdInGallery.tsx
+++ src/app/_components/AdInGallery.tsx
@@ -15,28 +15,29 @@ interface AdInGalleryProps {
 const AdInGallery: React.FC<AdInGalleryProps> = ({ loading, ads }) => {
   if (loading) {
     return (
-      <div className="w-[350px] h-[170px] flex justify-center col-start-2 row-start-2 items-center mx-auto grid-cols-2 grid-rows-2 rounded-lg">
+      <div className="w-[350px] h-[170px] flex  justify-center col-start-2 row-start-2 items-center mx-auto grid-cols-2 grid-rows-2 rounded-lg">
         <Spinner />
       </div>
     );
   }
   return (
     <>
-      {ads.length === 0 ? (
-        <div className="   col-start-2 row-start-2 text-nowrap flex flex-col items-center justify-center gap-2 ">
-        <h4 className="text-right text-base  text-black-primary  ">
+      {ads?.length === 0 ? (
+        
+        <div className="  col-start-2 row-start-2 text-nowrap flex flex-col items-center justify-center gap-2 ">
+        <h4 className="text-right text-base  text-black-primary dark:text-dark-white-primary  ">
 .نتیجه‌ای برای جستجوی شما پیدا نشد
 </h4>
-<span className="text-black-secondary text-[0.875rem]">
+<span className="text-black-secondary dark:text-dark-white-secondary text-[0.875rem]">
   .پیشنهاد می‌کنیم که دسته و فیلترهای انتخابی خود را بازبینی کنید
 </span>
         </div>
       ) : (
-        ads.map((ad) => (
+        ads?.map((ad) => (
           <Link
-            href={`/ads/${ad.id}`}
-            key={ad.id}
-            className="flex cursor-pointer shadow-[0_0_0_1px_rgba(0,0,0,0.12)] p-4 w-[350px] h-[170px] justify-between  border-spacing-1 transition-shadow rounded-sm "
+            href={`/ads/${ad?.id}`}
+            key={ad?.id}
+            className="flex cursor-pointer  dark:border  dark:border-[#2c2c2c] shadow-[0_0_0_1px_rgba(0,0,0,0.12)] p-4 w-[350px] h-[170px] justify-between  dark:border-spacing-1 transition-shadow rounded-sm "
           >
             <div className="flex w-[160px] h-full gap-1">
               {ad.img1 ? (
@@ -58,27 +59,27 @@ const AdInGallery: React.FC<AdInGalleryProps> = ({ loading, ads }) => {
               )}
               <FiMessageCircle
                 size={25}
-                className="self-end text-black-secondary"
+                className="self-end text-black-secondary dark:text-dark-white-secondary"
               />
             </div>
 
             <div className="flex flex-col justify-between w-[calc(100%-180px)]">
-              <h2 className="text-right text-black-primary text-base line-clamp-2">
+              <h2 className="text-right text-black-primary dark:text-dark-white-primary text-base line-clamp-2">
                 {ad.title}
               </h2>
               <div className="flex flex-col gap-2">
-                <span className="text-right text-black-secondary text-sm">
+                <span className="text-right text-black-secondary dark:text-dark-white-secondary text-sm">
                   {ad.status}
                 </span>
                 <div className="flex justify-start items-center flex-row-reverse gap-1">
-                  <span className="text-right text-black-secondary text-sm">
+                  <span className="text-right text-black-secondary dark:text-dark-white-secondary text-sm">
                     {formatPrice(ad.price.toString())}
                   </span>
-                  <span className="text-right text-black-secondary text-sm">
+                  <span className="text-right text-black-secondary dark:text-dark-white-secondary text-sm">
                     تومان
                   </span>
                 </div>
-                <span className="text-right text-black-primary text-sm">
+                <span className="text-right text-black-primary dark:text-dark-white-primary text-sm">
                   <PersianRelativeTime
                     createdAt={ad.created_at}
                     fontSize="text-sm"
