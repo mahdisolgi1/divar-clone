f817585f367d402b07fa99aa6be2040579a15db6
mahdi solgi <mahdi.solgi.dev@gmail.com>
2025-07-17 10:32:27 -0700

    fix(next.config): update Supabase hostname configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["wrqgqxhrblgwtcssvgyo.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wrqgqxhrblgwtcssvgyo.supabase.co",
+      },
+    ],
   },
 };
 
