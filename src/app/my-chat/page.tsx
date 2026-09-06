c5d2f8bb96a68cec559acaa4d4d86f28ef952cd9
mahdi solgi <vass1383@gmail.com>
2026-09-06 05:42:13 -0700

    refactor(Chat): testing a error

--- src/app/my-chat/page.tsx
+++ src/app/my-chat/page.tsx
@@ -1,8 +1,10 @@
 // import ChatBox from "../_components/ChatBox";
 
+import { div } from "framer-motion/client";
+
 const page: React.FC = () => {
   return (
-   
+   <div>s</div>
 {/* <ChatBox /> */}
   );
 };
