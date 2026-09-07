2d87b60edf9adf106e349678b8759e66d437d15a
mahdi solgi <vass1383@gmail.com>
2026-09-06 19:11:35 -0700

    fix(page\ads): fix an error related to useSearchParams

--- src/app/ads/page.tsx
+++ src/app/ads/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import Gallery from "../_components/Gallery";
 
 export default function AdsPage() {
   return (
     <div className="container mx-auto px-4 py-8">
+    <Suspense  fallback={<div>Loading...</div>} >
       <Gallery />
+    </Suspense>
     </div>
   );
 }
+ 
